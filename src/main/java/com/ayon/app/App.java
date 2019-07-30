cfbc395ced51a45aeb3bf32ee6f2eb1b809b073e
Nahiyan Kamal <noyanayihan@gmail.com>
2019-07-30 20:16:24 +0200

    adding tests

--- src/main/java/com/ayon/app/App.java
+++ src/main/java/com/ayon/app/App.java
@@ -1,92 +1,17 @@
 package com.ayon.app;
 
-import com.ayon.lombok.ex1.LombokExample;
-import com.ayon.lombok.ex2.LombokExample2;
-import com.ayon.lombok.ex3.LombokExample3;
-import com.ayon.lombok.ex4.LombokExample4;
-import com.ayon.lombok.ex5.LombokExample5;
-import com.ayon.lombok.ex6.LombokExample6;
-import com.ayon.lombok.ex8.LombokExample8;
+import com.ayon.service.RunningLomboks;
 
 public class App {
     public static void main(String[] args) {
-        example1();
-        example2();
-        example3();
-        example4();
-        example5();
-        example6();
-        example7();
-        example8();
-    }
-
-    public static void example1 () {
-        LombokExample ex1 = new LombokExample(10,"test",false);
-        System.out.println(ex1.getSentence());
-        System.out.println("Passed test example 1");
-    }
-
-    public static void example2 () {
-        LombokExample2 ex2 = new LombokExample2(10,"test",false);
-        ex2.setValue(true);
-        System.out.println(ex2.getSentence());
-
-        System.out.println("Passed test example 2");
-    }
-
-    public static void example3 () {
-
-        //AllArgsConstructor
-        LombokExample3 ex2 = new LombokExample3("sen",true,"as");
-
-        //RequiredArgsConstructor
-        LombokExample3 ex3 = new LombokExample3("sen");
-
-        System.out.println("Passed test example 3");
-    }
-
-    public static void example4 () {
-        //AllArgsConstructor
-        LombokExample4 ex1 = new LombokExample4("sen",true,"as");
-        System.out.println(ex1); //LombokExample4(sentence=sen, isValue=true)
-
-        //AllArgsConstructor
-        LombokExample4 ex2 = new LombokExample4("sen",true,"as");
-
-        System.out.println(ex1.equals(ex2)); //true
-
-        //NoArgsConstructor
-        LombokExample4 ex3 = new LombokExample4();
-        System.out.println(ex3);
-
-        System.out.println("Passed test example 4");
-    }
-
-    public static void example5 () {
-        //Builder
-        LombokExample5 ex1 = LombokExample5.builder().isValue(true).sentence("asdf").val("asd").build();
-
-        System.out.println("Passed test example 5");
-    }
-
-    //RequiredArgsConstructor, generates a constructor for all final fields, with parameter order same as field order
-    public static void example6 () {
-        //Builder
-        LombokExample6 ex1 = new LombokExample6("sentence");
-        System.out.println("Passed test example 6");
-    }
-
-    //RequiredArgsConstructor, generates a constructor for all final fields, with parameter order same as field order
-    public static void example7 () {
-        //Builder
-        LombokExample6 ex1 = new LombokExample6("sentence",true, "val");
-        System.out.println("Passed test example 7");
-    }
-
-    //NoArgsConstructor, generates a constructor without any fields
-    public static void example8 () {
-        //Builder
-        LombokExample8 ex1 = new LombokExample8();
-        System.out.println("Passed test example 8");
+        RunningLomboks examples = new RunningLomboks();
+        examples.tryExample1();
+        examples.tryExample2();
+        examples.tryExample3();
+        examples.tryExample4();
+        examples.tryExample5();
+        examples.tryExample6();
+        examples.tryExample7();
+        examples.tryExample8();
     }
 }
---
+++ src/main/java/com/ayon/service/RunningLomboksTest.java
@@ -0,0 +1,56 @@
+package com.ayon.service;
+
+import org.junit.Test;
+
+public class RunningLomboksTest {
+
+    @Test
+    public void tryExample1() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample1();
+    }
+
+
+    @Test
+    public void tryExample2() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample2();
+    }
+
+    @Test
+    public void tryExample3() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample3();
+    }
+
+    @Test
+    public void tryExample4() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample4();
+    }
+
+    @Test
+    public void tryExample5() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample5();
+    }
+
+    @Test
+    public void tryExample6() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample6();
+    }
+
+
+    @Test
+    public void tryExample7() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample7();
+    }
+
+    @Test
+    public void tryExample8() {
+        RunningLomboks r = new RunningLomboks();
+        r.tryExample8();
+    }
+}
